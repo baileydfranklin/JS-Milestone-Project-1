62a3aef5e1a10900464c92ecef7b7f161daa8a37
Bailey Franklin <baileydfranklin@gmail.com>
2023-02-19 11:09:44 -0700

    Got the cards to show up once button is clicked

---
+++ index.js
@@ -0,0 +1,35 @@
+//Use this URL for the API: https://www.deckofcardsapi.com/
+
+const DECK_URL =
+  "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+
+const button = document.querySelector("#dealButton");
+const player1 = document.querySelector("#player-1-conainer");
+
+button.addEventListener("click", async () => {
+
+    const deck = await shuffled();
+    const deckData = await deck.json();
+    const { deck_id } = deckData;
+    const card = await drawACard(deck_id, 2);
+    const cardData = await card.json();
+    const { cards } = cardData;
+    const card1 = cards[0];
+
+    const img = document.createElement("img");
+    img.setAttribute("src", card1.images.svg);
+    console.log({ img, card1 });
+    player1.append(img);
+    console.log({ cardData });
+});
+
+async function shuffled() {
+  const deck = await fetch(DECK_URL);
+  return deck;
+}
+
+async function drawACard(deckId, number) {
+  const CARD_URL = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=${number}`;
+  const card = await fetch(CARD_URL);
+  return card;
+}
